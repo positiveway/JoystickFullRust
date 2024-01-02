5d7e1281cae6a01969bf723a20bc2128a9ea3acc
Andrew <PositiveWay05@gmail.com>
2024-01-03 01:41:54 +0500

    process events

--- src/config.rs
+++ src/config.rs
@@ -5,19 +5,14 @@ use serde::Deserialize;
 lazy_static! {
     pub static ref GLOBAL_CONFIG: Config = Config::load();
 }
-// pub static GLOBAL_CONFIG: Config = Config{
-//     typing_layout: "".to_string(),
-//     buttons_layout: "".to_string(),
-//     finger_rotation: None,
-//     triggers_threshold_pct: 0,
-// };
 
 
 #[derive(Deserialize)]
 pub struct Config {
     pub typing_layout: String,
     pub buttons_layout: String,
-    pub finger_rotation: Option<u8>,
+    #[serde(default)]
+    pub finger_rotation: u8,
     pub triggers_threshold_pct: u8,
 }
 
@@ -26,7 +21,7 @@ impl Config {
         Config {
             typing_layout: "".to_string(),
             buttons_layout: "".to_string(),
-            finger_rotation: None,
+            finger_rotation: 0,
             triggers_threshold_pct: 0,
         }
     }
